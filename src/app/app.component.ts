88696b8fe36aa02c7b1c0b720a4becc39dcf4cb3
rshyam <rshyam@hala.uk>
2024-08-06 00:46:56 +0530

    auth code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'UP';
   accountNo: any;
-  constructor(){
-
-  }
-  fetchBill(){
+  constructor() {}
+  fetchBill() {
     let acNo = this.accountNo ? this.accountNo : '';
     let url = `https://www.uppclonline.com/dispatch/Portal/BD?accNo=${acNo}&dis=MVVNL`;
-    if(acNo){
-      this.openExternalLink(url);
+    if (acNo) {
+      if (localStorage.getItem('token')) {
+        this.openExternalLink(url);
+      } else {
+        this.openDialogue(url);
+      }
     } else {
-      alert('Enter the account Number!')
+      alert('Enter the account Number!');
+    }
+  }
+
+  openDialogue(url: string) {
+    const code = prompt('Enter Auth code:');
+    if (code === '80900') {
+      this.openExternalLink(url);
+      localStorage.setItem('token', code);
+      // Your code for proceeding
+    } else {
+      alert('Invalid code.');
     }
   }
 
