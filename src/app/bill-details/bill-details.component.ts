0c8d157372b7c6c16ef28e767620238bd8aa05ef
rshyam@hala.uk <rshyam@hala.uk>
2025-02-22 17:32:46 +0530

    UI Changes

--- src/app/bill-details/bill-details.component.ts
+++ src/app/bill-details/bill-details.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-bill-details',
   templateUrl: './bill-details.component.html',
-  styleUrls: ['./bill-details.component.scss']
+  styleUrls: ['./bill-details.component.scss'],
 })
-export class BillDetailsComponent {
+export class BillDetailsComponent implements AfterViewInit {
   accountNo: any;
-  
+  pageType = '';
+  @ViewChild('accountInput') accountInput!: ElementRef;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+
+    // Subscribe to query parameters
+    this.route.queryParams.subscribe((params) => {
+      this.pageType = params['actionType'] || '';
+      // You can use the parameters as needed
+      console.log('ID:', this.pageType);
+      setTimeout(() => {
+        this.focusInput();
+      }, 10);
+    });
+  }
+
+  ngAfterViewInit() {
+    this.focusInput();
+  }
+
+  submit() {
+    if (this.pageType === '' || this.pageType === 'bill-details') {
+      this.fetchBill();
+    } else {
+      this.fetchReceipt();
+    }
+  }
+
   fetchBill() {
     let acNo = this.accountNo ? this.accountNo : '';
     let url = `https://www.uppclonline.com/dispatch/Portal/BD?accNo=${acNo}&dis=MVVNL`;
@@ -35,5 +65,29 @@ export class BillDetailsComponent {
 
   openExternalLink(url: string) {
     window.open(url, '_blank'); // Opens in a new tab
+  }
+
+  fetchReceipt() {
+    let acNo = this.accountNo ? this.accountNo : '';
+    let url = `https://www.uppclonline.com/dispatch/Portal/appmanager/uppcl/PrintReceiptServlet?accountNo=${acNo}&reportName=paymentReceipt&displayReportName=Payment%20Receipt&discomName=MVVNL`;
+    if (acNo) {
+      if (localStorage.getItem('token')) {
+        this.openExternalLink(url);
+      } else {
+        this.openDialogue(url);
+      }
+    } else {
+      alert('Enter the account Number!');
+    }
+  }
+
+  clear(){
+    this.accountNo = '';
+    this.focusInput();
+  }
+
+  focusInput(){
+    this.accountInput?.nativeElement?.focus();
+
   }
 }
